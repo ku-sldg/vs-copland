0f062eeb80fa30fa6cf7f9adea56183982901c42
Adam Petz <ampetz@ku.edu>
2025-09-11 12:22:54 -0500

    initial rust-sitter project skeleton

---
+++ rust-sitter-copland/copland_concrete/src/copland_concrete.rs
@@ -0,0 +1,113 @@
+#[rust_sitter::grammar("copland_concrete")]
+pub mod grammar {
+    #[rust_sitter::language]
+    #[derive(PartialEq, Eq, Debug)]
+    // r"^[a-z][a-zA-Z0-9_]*$"
+    pub enum Expression {
+        Number(#[rust_sitter::leaf(pattern = r"[a-z][a-zA-Z0-9_]*", transform = |v| v.parse().unwrap())] String),
+        #[rust_sitter::prec_left(1)]
+        Sub(
+            Box<Expression>,
+            #[rust_sitter::leaf(text = "-")] (),
+            Box<Expression>,
+        ),
+        #[rust_sitter::prec_left(2)]
+        Mul(
+            Box<Expression>,
+            #[rust_sitter::leaf(text = "*")] (),
+            Box<Expression>,
+        ),
+    }
+
+    #[rust_sitter::extra]
+    struct Whitespace {
+        #[rust_sitter::leaf(pattern = r"\s")]
+        _whitespace: (),
+    }
+}
+
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use grammar::Expression;
+
+    #[wasm_bindgen_test::wasm_bindgen_test]
+    #[test]
+    fn successful_parses() {
+        assert_eq!(grammar::parse("1").unwrap(), Expression::Number(1));
+
+        assert_eq!(grammar::parse(" 1").unwrap(), Expression::Number(1));
+
+        assert_eq!(
+            grammar::parse("1 - 2").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Number(1)),
+                (),
+                Box::new(Expression::Number(2))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 - 2 - 3").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Sub(
+                    Box::new(Expression::Number(1)),
+                    (),
+                    Box::new(Expression::Number(2))
+                )),
+                (),
+                Box::new(Expression::Number(3))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 - 2 * 3").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Number(1)),
+                (),
+                Box::new(Expression::Mul(
+                    Box::new(Expression::Number(2)),
+                    (),
+                    Box::new(Expression::Number(3))
+                ))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 * 2 * 3").unwrap(),
+            Expression::Mul(
+                Box::new(Expression::Mul(
+                    Box::new(Expression::Number(1)),
+                    (),
+                    Box::new(Expression::Number(2))
+                )),
+                (),
+                Box::new(Expression::Number(3))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 * 2 - 3").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Mul(
+                    Box::new(Expression::Number(1)),
+                    (),
+                    Box::new(Expression::Number(2))
+                )),
+                (),
+                Box::new(Expression::Number(3))
+            )
+        );
+    }
+
+    #[test]
+    fn failed_parses() {
+        insta::assert_debug_snapshot!(grammar::parse("1 + 2"));
+        insta::assert_debug_snapshot!(grammar::parse("1 - 2 -"));
+        insta::assert_debug_snapshot!(grammar::parse("a1"));
+        insta::assert_debug_snapshot!(grammar::parse("1a"));
+    }
+    
+}
+    */
---
+++ rust-sitter-copland/copland_concrete/src/main.rs
@@ -0,0 +1,87 @@
+use std::io::Write;
+
+use codemap::CodeMap;
+use codemap_diagnostic::{ColorConfig, Diagnostic, Emitter, Level, SpanLabel, SpanStyle};
+use rust_sitter::errors::{ParseError, ParseErrorReason};
+
+mod copland_concrete;
+
+fn convert_parse_error_to_diagnostics(
+    file_span: &codemap::Span,
+    error: &ParseError,
+    diagnostics: &mut Vec<Diagnostic>,
+) {
+    match &error.reason {
+        ParseErrorReason::MissingToken(tok) => diagnostics.push(Diagnostic {
+            level: Level::Error,
+            message: format!("Missing token: \"{tok}\""),
+            code: Some("S000".to_string()),
+            spans: vec![SpanLabel {
+                span: file_span.subspan(error.start as u64, error.end as u64),
+                style: SpanStyle::Primary,
+                label: Some(format!("missing \"{tok}\"")),
+            }],
+        }),
+
+        ParseErrorReason::UnexpectedToken(tok) => diagnostics.push(Diagnostic {
+            level: Level::Error,
+            message: format!("Unexpected token: \"{tok}\""),
+            code: Some("S000".to_string()),
+            spans: vec![SpanLabel {
+                span: file_span.subspan(error.start as u64, error.end as u64),
+                style: SpanStyle::Primary,
+                label: Some(format!("unexpected \"{tok}\"")),
+            }],
+        }),
+
+        ParseErrorReason::FailedNode(errors) => {
+            if errors.is_empty() {
+                diagnostics.push(Diagnostic {
+                    level: Level::Error,
+                    message: "Failed to parse node".to_string(),
+                    code: Some("S000".to_string()),
+                    spans: vec![SpanLabel {
+                        span: file_span.subspan(error.start as u64, error.end as u64),
+                        style: SpanStyle::Primary,
+                        label: Some("failed".to_string()),
+                    }],
+                })
+            } else {
+                for error in errors {
+                    convert_parse_error_to_diagnostics(file_span, error, diagnostics);
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+
+    loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let input = input.trim();
+        if input.is_empty() {
+            break;
+        }
+
+        match copland_concrete::grammar::parse(input) {
+            Ok(expr) => println!("{expr:?}"),
+            Err(errs) => {
+                let mut codemap = CodeMap::new();
+                let file_span = codemap.add_file("<input>".to_string(), input.to_string());
+                let mut diagnostics = vec![];
+                for error in errs {
+                    convert_parse_error_to_diagnostics(&file_span.span, &error, &mut diagnostics);
+                }
+
+                let mut emitter = Emitter::stderr(ColorConfig::Always, Some(&codemap));
+                emitter.emit(&diagnostics);
+            }
+        };
+    }
+}
